349b875d6cbffc6b023874d5b27468fd92be7c5c
yutaroe <yucl895@gmail.com>
2021-10-25 17:51:58 +0900

    修正

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'rest_auth.registration',
+    'corsheaders',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,6 +56,8 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'newapp.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,6 +65,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:3001'
 ]
 
 ROOT_URLCONF = 'project1.urls'
